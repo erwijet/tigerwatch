d6c0d231af58654bfdd3f3a174d59b3aa3638f56
erwijet <tsh6656@rit.edu>
2021-10-27 00:42:07 -0400

    unstable react app

--- rattman/src/App.tsx
+++ rattman/src/App.tsx
@@ -9,24 +9,40 @@ function App() {
   // eslint-disable-next-line
   const [ skey, setSkey ] = useStickyState<string>('', 'skey');
   const [ spendingData, setSpendingData ] = useState([]);
-  
-  async function syncSpendingData() {
+
+  async function validateSKey() {
+    return new Promise<void>(async (resolve) => {
       const res = await fetch(`https://vps.erwijet.com/data/${skey}`);
       if (res.status === 401) {
         const socket = io('https://vps.erwijet.com');
-
+    
         socket.on('message', (msg) => {
           window.open('https://vps.erwijet.com/' + msg);
+          socket.off('message'); // only listen to first message
         });
 
-        socket.on('skey', async ({ skey }) => {
-          setSkey(skey);
-          alert(skey);
-          syncSpendingData();
+        socket.on('skey', async ({ skey: newSkey }) => {
+          socket.disconnect(); // should disconnect from server, but we ensure it is disconnected here (WS uses a lot of battery)
+          
+          while (skey != newSkey) {
+            console.log({skey, newSkey});
+            setSkey(newSkey);
+          }
+
+          resolve();
         });
-      } else {
-        setSpendingData(await res.json());
-      }
+      } else resolve();
+    });
+  }
+
+  async function syncSpendingData() {
+    await validateSKey();
+    const res = await fetch(`https://vps.erwijet.com/data/${skey}`);
+    if (res.status === 401) {
+      throw 'skey should have been validated but server returned 401\n' + 'skey: ' + skey + '\n' + JSON.stringify(res); // should *never* happen
+    } else {
+      setSpendingData(await res.json());
+    }
   }
   
   return (
