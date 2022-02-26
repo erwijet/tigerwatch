6da5e7487478170b15f5d42b3d2da2de30669b2b
Tyler Holewinski <tsh6656@rit.edu>
2022-02-25 22:33:32 -0500

    fix type issues (removed react-scripts)

--- rattman/vite.config.ts
+++ rattman/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
     },
     plugins: [
         react(),
+        VitePWA(),
         svgrPlugin({
             svgrOptions: {
                 icon: true,
